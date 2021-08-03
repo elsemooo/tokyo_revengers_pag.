25b4bb2f21e5dea276aaaa7c19a5fa985e7e1d2e
khalid samanamud <mathiaskalid@gmail.com>
2021-08-03 18:26:47 -0500

    meta

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@ function toggleMenu(){
 }
 openMenu.addEventListener("click",toggleMenu);
 closeMenu.addEventListener("click",toggleMenu);
+
+const manga1 = document.querySelector("#manga1");
+const pdfmanga1 = document.querySelector("#pdf1");
+// function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// manga1.addEventListener("click",console.log("pene"));
