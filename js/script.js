61d2779bfbfb8245df9e825ca1a38a93026bec40
khalid samanamud <mathiaskalid@gmail.com>
2021-08-06 09:44:26 -0500

    index terminado, managa solo se puede leer hasta el tomo 7

--- js/script.js
+++ js/script.js
@@ -6,31 +6,79 @@ function toggleMenu(){
 }
 openMenu.addEventListener("click",toggleMenu);
 closeMenu.addEventListener("click",toggleMenu);
-
+//manga 1
 const manga1 = document.querySelector("#manga1");
 const pdfmanga1 = document.querySelector("#pdf1");
-// function
+const closePdf1 = document.querySelector("#closePdf1");
 
+function toggleMenuManga1(){
+    pdfmanga1.classList.toggle("active");
+    closePdf1.classList.toggle("active");
+}
+manga1.addEventListener("click", toggleMenuManga1);
+closePdf1.addEventListener("click", toggleMenuManga1);
+//manga 2
+const manga2 = document.querySelector("#manga2");
+const pdfmanga2 = document.querySelector("#pdf2");
+const closePdf2 = document.querySelector("#closePdf2");
+function toggleMenuManga2(){
+    pdfmanga2.classList.toggle("active");
+    closePdf2.classList.toggle("active");
+}
+manga2.addEventListener("click", toggleMenuManga2);
+closePdf2.addEventListener("click", toggleMenuManga2);
+//manga 3
+const manga3 = document.querySelector("#manga3");
+const pdfmanga3 = document.querySelector("#pdf3");
+const closePdf3 = document.querySelector("#closePdf3");
 
+function toggleMenuManga3(){
+    pdfmanga3.classList.toggle("active");
+    closePdf3.classList.toggle("active");
+}
+manga3.addEventListener("click", toggleMenuManga3);
+closePdf3.addEventListener("click", toggleMenuManga3);
+//manga 4
+const manga4 = document.querySelector("#manga4");
+const pdfmanga4 = document.querySelector("#pdf4");
+const closePdf4 = document.querySelector("#closePdf4");
 
+function toggleMenuManga4(){
+    pdfmanga4.classList.toggle("active");
+    closePdf4.classList.toggle("active");
+}
+manga4.addEventListener("click", toggleMenuManga4);
+closePdf4.addEventListener("click", toggleMenuManga4);
+//manga 5
+const manga5 = document.querySelector("#manga5");
+const pdfmanga5 = document.querySelector("#pdf5");
+const closePdf5 = document.querySelector("#closePdf5");
 
+function toggleMenuManga5(){
+    pdfmanga5.classList.toggle("active");
+    closePdf5.classList.toggle("active");
+}
+manga5.addEventListener("click", toggleMenuManga5);
+closePdf5.addEventListener("click", toggleMenuManga5); 
+//manga 6
+const manga6 = document.querySelector("#manga6");
+const pdfmanga6 = document.querySelector("#pdf6");
+const closePdf6 = document.querySelector("#closePdf6");
 
+function toggleMenuManga6(){
+    pdfmanga6.classList.toggle("active");
+    closePdf6.classList.toggle("active");
+}
+manga6.addEventListener("click", toggleMenuManga6);
+closePdf6.addEventListener("click", toggleMenuManga6);
+//manga 7
+const manga7 = document.querySelector("#manga7");
+const pdfmanga7 = document.querySelector("#pdf7");
+const closePdf7 = document.querySelector("#closePdf7");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// manga1.addEventListener("click",console.log("pene"));
+function toggleMenuManga7(){
+    pdfmanga7.classList.toggle("active");
+    closePdf7.classList.toggle("active");
+}
+manga7.addEventListener("click", toggleMenuManga7);
+closePdf7.addEventListener("click", toggleMenuManga7);
